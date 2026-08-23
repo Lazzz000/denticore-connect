a9ce73283485ccebf0158abb95e9830f82d64020
Miguel Lazo <190323837+Lazzz000@users.noreply.github.com>
2026-08-23 11:03:56 -0500

    chore(ios): prepara delegado de notificaciones para concurrencia

--- ios/DentiCoreConnect/DentiCoreConnect/AppDelegate.swift
+++ ios/DentiCoreConnect/DentiCoreConnect/AppDelegate.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 import CoreData
-import UserNotifications
+@preconcurrency import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
