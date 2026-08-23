697f2de7b70f8bfe6b251df45e33d13fbdc91521
Miguel Lazo <190323837+Lazzz000@users.noreply.github.com>
2026-08-22 19:30:39 -0500

    feat(backend): agrega flujo móvil de citas

---
+++ backend/src/main/java/com/cibertec/denticore/movil/dto/OdontologoMovilDTO.java
@@ -0,0 +1,9 @@
+package com.cibertec.denticore.movil.dto;
+
+public record OdontologoMovilDTO(
+        Integer id,
+        String nombres,
+        String apellidos,
+        String nombreCompleto,
+        String cop) {
+}
