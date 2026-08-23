e135d15b598c8d25685f9fb4bdbf3063bdcbde2c
Miguel Lazo <190323837+Lazzz000@users.noreply.github.com>
2026-08-23 10:30:30 -0500

    style(ios): aplica el tema global

--- ios/DentiCoreConnect/DentiCoreConnect/AppDelegate.swift
+++ ios/DentiCoreConnect/DentiCoreConnect/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        DentiCoreTheme.configureGlobalAppearance()
         return true
     }
 
@@ -78,4 +78,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 }
-
